81b1f69838ccdc8d8cdfd315648bdc2a43658d42
Ciaran Murphy <ciaran.murphy@docosoft.com>
2022-06-16 20:16:24 +0100

    Changes Up to Parameter

--- DemoCode.cs
+++ DemoCode.cs
@@ -5,9 +5,35 @@ namespace DemoBasic
 {
     public class DemoCode
     {
+        public delegate void TestDelegate();
+        public delegate bool TestBoolDelegate(int i);
+
+        private TestDelegate testDelegate;
+        private TestBoolDelegate testBoolDelegate;
+
         public void Start()
         {
+            //testDelegate = MyTestFunction;
+            //testDelegate();
 
+            testBoolDelegate = MyBoolFunction;
+            Console.WriteLine(testBoolDelegate(10));
+
+        }
+
+        private void MyTestFunction()
+        {
+            Console.WriteLine("This is my test Function");
+        }
+
+        private void MySecondTestFunction()
+        {
+            Console.WriteLine("This is my second function");
+        }
+
+        private bool MyBoolFunction(int i)
+        {
+            return i > 5;
         }
     }
 }
